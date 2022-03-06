8a28d7e0e602c1a9d6d350e346b482d4429b321f
LianNidam <liannidam04@gmail.com>
2022-03-06 13:22:05 +0200

    Change the color of the letters and audio of drums when you press the letters

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ var numOfAllDrumsButtons= document.querySelectorAll(".drum").length;
 for( var i=0; i<numOfAllDrumsButtons ; i++){
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-      alert("I got clicked!");
+    this.style.color="white";
+
   });
 
 }
+
+
+
+//    var audio = new Audio('sounds/tom-1.mp3');
+//    audio.play();
